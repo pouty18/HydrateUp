d3412e58f33d42d2b2f603adb05bab25260447b1
Richard Poutier <rcpoutier@gmail.com>
2018-11-30 12:18:25 -0800

    Initial Commit

---
+++ HydrateUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HydrateUp
+//
+//  Created by Richard Poutier on 11/30/18.
+//  Copyright © 2018 Richard Poutier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
